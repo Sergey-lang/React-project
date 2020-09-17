6d6d55e719c0bd45ed65ab7817c852ff3f2430f3
Sergey-Lang <63201050+Sergey-lang@users.noreply.github.com>
2020-09-17 18:03:46 +0300

    Updates

---
+++ precache-manifest.b41d210290fa63a3f9efee55e6219772.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e69a969b38ae6aa7344995f6117cd7b9",
+    "url": "/Social-JS/index.html"
+  },
+  {
+    "revision": "775e0792cae9216d7f02",
+    "url": "/Social-JS/static/css/main.3319f719.chunk.css"
+  },
+  {
+    "revision": "3bb3fc4e69562683a6f3",
+    "url": "/Social-JS/static/js/2.acc31a20.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Social-JS/static/js/2.acc31a20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "775e0792cae9216d7f02",
+    "url": "/Social-JS/static/js/main.e83b2259.chunk.js"
+  },
+  {
+    "revision": "4521d2ef42f3f0395a62",
+    "url": "/Social-JS/static/js/runtime-main.e4f161a1.js"
+  },
+  {
+    "revision": "d5c02aa6c461a18d08978be560c78740",
+    "url": "/Social-JS/static/media/preloader.d5c02aa6.gif"
+  },
+  {
+    "revision": "abb5b607f8aee2bd5af6968a9928e20e",
+    "url": "/Social-JS/static/media/user.abb5b607.png"
+  }
+]);
