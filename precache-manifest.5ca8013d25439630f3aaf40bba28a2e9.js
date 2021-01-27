2e181e36a544580717f27a457180c566c420adeb
Sergey-lang <sergei.kuharyonok@yandex.by>
2021-01-28 00:53:28 +0300

    Updates

---
+++ precache-manifest.5ca8013d25439630f3aaf40bba28a2e9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a9bf816ce8bbdce4cbd2adcb097d822",
+    "url": "/Social-JS/index.html"
+  },
+  {
+    "revision": "eba2a879450e274724b4",
+    "url": "/Social-JS/static/css/3.8c1dcbfa.chunk.css"
+  },
+  {
+    "revision": "2a39bc3434d821fc34e4",
+    "url": "/Social-JS/static/css/4.3ce81417.chunk.css"
+  },
+  {
+    "revision": "d54ef65176192c631f6b",
+    "url": "/Social-JS/static/css/5.c05d8dc6.chunk.css"
+  },
+  {
+    "revision": "5367011b66058d5da10d",
+    "url": "/Social-JS/static/css/6.b69e186c.chunk.css"
+  },
+  {
+    "revision": "ec13d34b7609d583eb30",
+    "url": "/Social-JS/static/css/7.4a82d983.chunk.css"
+  },
+  {
+    "revision": "d81420d67f3bbca6ea5e",
+    "url": "/Social-JS/static/css/main.e8356e93.chunk.css"
+  },
+  {
+    "revision": "a0fc5899abc07df7f23a",
+    "url": "/Social-JS/static/js/0.3eab6f88.chunk.js"
+  },
+  {
+    "revision": "eba2a879450e274724b4",
+    "url": "/Social-JS/static/js/3.0fbaa325.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/Social-JS/static/js/3.0fbaa325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a39bc3434d821fc34e4",
+    "url": "/Social-JS/static/js/4.b97d48ed.chunk.js"
+  },
+  {
+    "revision": "d54ef65176192c631f6b",
+    "url": "/Social-JS/static/js/5.d37f70ff.chunk.js"
+  },
+  {
+    "revision": "5367011b66058d5da10d",
+    "url": "/Social-JS/static/js/6.e7d332b0.chunk.js"
+  },
+  {
+    "revision": "ec13d34b7609d583eb30",
+    "url": "/Social-JS/static/js/7.b3b2e1fb.chunk.js"
+  },
+  {
+    "revision": "d81420d67f3bbca6ea5e",
+    "url": "/Social-JS/static/js/main.4587f120.chunk.js"
+  },
+  {
+    "revision": "65f1569cf1ee3111f830",
+    "url": "/Social-JS/static/js/runtime-main.6724659f.js"
+  },
+  {
+    "revision": "e67e358c72e30a5ec593534154b91ea6",
+    "url": "/Social-JS/static/media/avatar.e67e358c.png"
+  },
+  {
+    "revision": "d5c02aa6c461a18d08978be560c78740",
+    "url": "/Social-JS/static/media/preloader.d5c02aa6.gif"
+  },
+  {
+    "revision": "abb5b607f8aee2bd5af6968a9928e20e",
+    "url": "/Social-JS/static/media/user.abb5b607.png"
+  }
+]);
